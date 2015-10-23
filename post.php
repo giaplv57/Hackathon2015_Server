da2f14d748e02a3d9f52dffffc9588bbb7a288b1
giaplv57 <giaplvk57@gmail.com>
2015-10-23 23:05:00 +0700

    nope

--- post.php
+++ post.php
@@ -1,14 +1,13 @@
 <?php
   include("common.php");
   header("Content-Type:application/json");
-  $receivedRequest = json_decode(file_get_contents('php://input'), true);
+  $receivedRequest = $_POST;
   $result = array();
-  // var_dump($receivedRequest);
   if($receivedRequest['action'] === "add"){
-    echo addEvent($receivedRequest['data']);
+    echo addEvent($receivedRequest);
   }
   if($receivedRequest['action'] === "get"){
-    echo getEvent($receivedRequest['data']);
+    echo getEvent($receivedRequest);
   }
 
   function addEvent($eventData){
@@ -21,9 +20,8 @@
     $picture = $eventData['picture'];
     $location = $eventData['location'];
     $reference = $eventData['title'];
-
-    mysqli_query($con, "INSERT INTO events (id,title,tagID,time,picture,location,reference) VALUES ('$eventID','$title','$tagID','$time','$picture','$location','$reference')");
-
+    $content = $eventData['content'];
+    mysqli_query($con, "INSERT INTO events (id,title,content,tagID,time,picture,location,reference) VALUES ('$eventID','$title','$content','$tagID','$time','$picture','$location','$reference')");
     mysqli_query($con, "INSERT INTO eventList (userID, eventID) VALUES ('$userID','$eventID')");
     $result['message'] = "success";
     $result['eventID'] = $eventID;
@@ -35,5 +33,6 @@
     $eventID = $eventData['eventID'];
     $getEvent = mysqli_query($con, "SELECT * FROM `events` where id='$eventID'");  
     $event = mysqli_fetch_array($getEvent);
-    echo json_encode($event);
+    return json_encode($event);
   }
+?>
